2e6d3499412a3d14abb4727aadb6273bb7fd9ddf
Adam Ohlson <adam.ohlson0531@gmail.com>
2024-12-02 15:53:01 +0100

    second commit

--- server.js
+++ server.js
@@ -50,3 +50,8 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
+const port = process.env.PORT || 5000;  // Om det inte finns någon PORT-miljövariabel, använd 5000 lokalt
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
